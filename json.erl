f7c3171530137eab7e8317b7b20ec5264ebbc0a7
garzoc <ost@airmail.cc>
2016-12-26 17:09:04 +0100

    decode now supports nested lists

--- json.erl
+++ json.erl
@@ -1,6 +1,6 @@
 -module(json).
 
--export([decode/1,encode/1,list_to_array/2]).
+-export([decode/1,encode/1,list_to_array/1,print/1]).
 -define(leftBrack,32).
 
 
@@ -63,13 +63,31 @@ encode(L)->list_to_atom("{"++toJson(L)).
 
 %Reverse the process
 
+
 %reverse a list that has been turned into a string back to a list
-list_to_array(Arr,[44|Xs])->[Arr]++list_to_array([],Xs);
-list_to_array(Arr,[93|_])->[Arr];
-list_to_array(Arr,[91|Xs])->list_to_array(Arr,Xs);
-list_to_array(Arr,[X|Xs])->list_to_array(Arr++[X],Xs);
+%list_to_array2(Arr,[44|Xs],NestCount)->[Arr]++[list_to_array2([],Xs,NestCount)];
+%list_to_array2(Arr,[93|_],1)->[Arr];
+%list_to_array2(Arr,[93|Xs],NestCount)->list_to_array2([Arr],Xs,NestCount-1);
+%list_to_array2(_,[91|Xs],NestCount)->list_to_array2([],Xs,NestCount+1);
+%list_to_array2(Arr,[X|Xs],NestCount)->list_to_array2(Arr++[X],Xs,NestCount);
 %????????
-list_to_array(_,[])->[].
+%list_to_array2(_,[],_)->[].
+
+print(Data)->io:format("~p~n",[Data]).
+%reverse a list that has been turned into a string back to a list
+list_to_array(Arr,[44|Xs],1)->{Value,_}=json_find_value_type(Arr),{Next,Tail}=list_to_array([],Xs,1),{ [Value]++Next,Tail};
+list_to_array(Arr,[93|Xs],1)->{Value,_}=json_find_value_type(Arr),{[Value],Xs};
+list_to_array(Arr,[93|Xs],NestCount)->list_to_array(Arr++[93],Xs,NestCount-1);
+list_to_array(_,[91|Xs],0)->list_to_array2([],Xs,1);
+list_to_array(Arr,[91|Xs],NestCount)->list_to_array(Arr++[91],Xs,NestCount+1);
+list_to_array(Arr,[X|Xs],NestCount)->list_to_array(Arr++[X],Xs,NestCount).
+%????????
+%list_to_array2(_,[],_)->print("termination warning"),[].
+
+list_to_array(Array)->list_to_array([],Array,0).
+
+%json:decode("{\"hej\":[[11],10]}").
+%json:list_to_array2("[[10],10]").
 
 %key is always string might be uneccasary function
 json_key_string(Key,[34|Xs])->{Key,Xs};
@@ -77,14 +95,12 @@ json_key_string(Key,[X|Xs])->json_key_string(Key++[X],Xs).
 
 %process String
 json_find_value(Value,34,[34|Xs])->{Value,Xs};
-%process list
-json_find_value(Value,93,[93|Xs])->case {[json_find_value_type(S)||S<-list_to_array([],"["++Value++"]")],Xs} of {[[]],Tail}->{[],Tail}; Else->Else end;
 %process value
 json_find_value(Value,32,[32|Xs])->{case re:run(Value,"^[0-9]*$") of nomatch-> Value;_->list_to_integer(Value) end,Xs};
 json_find_value(Value,32,[44|Xs])->{case re:run(Value,"^[0-9]*$") of nomatch-> Value;_->list_to_integer(Value) end,","++Xs};
 json_find_value(Value,32,[125|Xs])->{case re:run(Value,"^[0-9]*$") of nomatch-> Value;_->list_to_integer(Value) end,"}"++Xs};
 %warning this ones doesn't retunr a tuple
-json_find_value(Value,32,[])->case re:run(Value,"^[0-9]*$") of nomatch-> Value;_->list_to_integer(Value) end;
+json_find_value(Value,32,[])->{case re:run(Value,"^[0-9]*$") of nomatch-> Value;_->list_to_integer(Value) end,[]};
 %process push to value placeholder and loop
 json_find_value(Value,Type,[X|Xs])->json_find_value(Value++[X],Type,Xs).
 
@@ -96,13 +112,14 @@ json_find_value_type([32|Xs])->json_find_value_type(Xs);
 %find nested object
 json_find_value_type([123|Xs])->json_find_brack([123|Xs]);
 %value is list return ->{Value found , remaining list}
-json_find_value_type([91|Xs])->json_find_value([],93,Xs);
+json_find_value_type([91|Xs])->list_to_array2([91|Xs]);%json_find_value([],93,Xs),
 %value is number return ->{Value found , remaining list}
 json_find_value_type([X|Xs])->json_find_value([X],32,Xs);
 
 json_find_value_type([])->[].
 
 %look for colon
+%[JSON structure verfications]==========================================
 json_find_colon([58|Xs])->json_find_value_type(Xs);
 json_find_colon([_|Xs])->json_find_colon(Xs).
 
@@ -115,7 +132,8 @@ json_find_end([X|Xs])->io:format("~p~n",[X]),json_find_end(Xs).
 
 %this might also be an unessacary functions that can be moved down to json_find_bracket
 json_find_qote([34|Xs])->{Key,String}=json_key_string([],Xs),{Value,String2}=json_find_colon(String),{FieldEnd,Tail}=json_find_end(String2),{[{list_to_atom(Key),Value}]++FieldEnd,Tail};
-json_find_qote([_|Xs])->json_find_qote(Xs).
+json_find_qote([_|Xs])->json_find_qote(Xs);
+json_find_qote([])->{[],[]}.
 
 json_find_brack([123|Xs])->json_find_qote(Xs);
 json_find_brack([X|Xs])->io:format("~p~n",[X]),json_find_brack(Xs);
